2147e67a6e085462595f146ad744ae15b153ed9c
Kevin Sheehan <kshhn12@gmail.com>
2024-12-14 12:12:18 -0500

    serialization console app compelete

---
+++ backend-development/module-3/SerializationConsoleApp/Person.cs
@@ -0,0 +1,4 @@
+public class Person {
+    public string? UserName { get; set; }
+    public int UserAge { get; set; }
+}
---
+++ backend-development/module-3/SerializationConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.IO;
+using System.Xml.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Person samplePerson = new Person { UserName = "test", UserAge = 21};
+
+        //1. Binary serialization
+        using (FileStream fs = new FileStream("person.dat", FileMode.Create))
+        {
+            BinaryWriter writer = new BinaryWriter(fs);
+            writer.Write(samplePerson.UserName);
+            writer.Write(samplePerson.UserAge);
+        }
+
+        Console.WriteLine("Binary serialization complete");
+
+        //2. XML serialization
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+        using (StreamWriter writer = new StreamWriter("person.xml"))
+        {
+            xmlSerializer.Serialize(writer, samplePerson);
+        }
+
+        //3. JSON serialization
+        string jsonString = JsonSerializer.Serialize(samplePerson);
+
+        File.WriteAllText("person.json", jsonString);
+        Console.WriteLine("JSON serialization complete.");
+    }
+}
