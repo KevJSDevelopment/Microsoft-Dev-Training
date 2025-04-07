e310d5d1fbae4759579d6dbeee7fe82aaa1e4633
Kevin Sheehan <kshhn12@gmail.com>
2025-04-06 20:01:58 -0400

    initial DSA setup

---
+++ DSA-Practice/DataStructures/Programs.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructures;
+
+public class Program
+{
+    static void BasicListMethod()
+    {
+        
+    }
+
+    static void LinkedListMethod()
+    {
+        LinkedList<string> sortedCards = new LinkedList<string>();
+
+        sortedCards.AddLast("Jack of Clubs");
+        sortedCards.AddLast("Queen of Diamonds");
+        sortedCards.AddLast("King of Hearts");
+        sortedCards.AddLast("Ace of Spades");
+
+        Console.WriteLine("\nSorted Card Deck:");
+
+        foreach(string card in sortedCards)
+        {
+            Console.WriteLine(card);
+        }
+
+        sortedCards.Remove("Jack of Clubs");
+
+        Console.WriteLine("\nUpdated Sorted Card Deck:");
+        foreach(string card in sortedCards)
+        {
+            Console.WriteLine(card);
+        }
+    }
+}
