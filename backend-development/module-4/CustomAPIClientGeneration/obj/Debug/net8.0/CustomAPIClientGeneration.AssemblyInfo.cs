88ecc95dd0fea338cd11e979127a113a7a87f454
Kevin Sheehan <kshhn12@gmail.com>
2025-04-08 09:18:14 -0400

    Remove obj folders from tracking

--- backend-development/module-4/CustomAPIClientGeneration/obj/Debug/net8.0/CustomAPIClientGeneration.AssemblyInfo.cs
+++ backend-development/module-4/CustomAPIClientGeneration/obj/Debug/net8.0/CustomAPIClientGeneration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomAPIClientGeneration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12448ccd545b938853c4ae933d48a2ea91bea4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e310d5d1fbae4759579d6dbeee7fe82aaa1e4633")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomAPIClientGeneration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomAPIClientGeneration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
