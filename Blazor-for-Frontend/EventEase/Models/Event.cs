8869e210daf7f5b25fcdda5a39e229e01bd2348d
Kevin Sheehan <kshhn12@gmail.com>
2025-04-11 19:40:05 -0400

    events displaying on home page

---
+++ Blazor-for-Frontend/EventEase/Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace EventEase.Models;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public string Location { get; set; }
+}
