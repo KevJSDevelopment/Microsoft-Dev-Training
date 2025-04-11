8e7b91ddf05df1651e5515399cc521e7d1d63d9c
Kevin Sheehan <kshhn12@gmail.com>
2025-04-11 17:57:09 -0400

    module 2 done

--- Blazor-for-Frontend/FeedbackApp/Program.cs
+++ Blazor-for-Frontend/FeedbackApp/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MyBlazorApp;
+using FeedbackApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton<DataService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<ApiService>();
 
 await builder.Build().RunAsync();
---
+++ Blazor-for-Frontend/MyBlazorApp/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBlazorApp;
+
+public class DataService
+{
+    public async Task<List<string>> GetProductsAsync()
+    {
+        await Task.Delay(500);
+        return new List<string>{ "Product 1", "Product 2", "Product 3" };   
+    }
+}
