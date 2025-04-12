2a2d690e5e2f5b1b62f69be9ebe05877ee510402
Kevin Sheehan <kshhn12@gmail.com>
2025-04-12 10:00:55 -0400

    database integration 1st project done

--- DatabaseIntegration/EFCoreModelApp/Program.cs
+++ DatabaseIntegration/EFCoreModelApp/Program.cs
@@ -1,3 +1,33 @@
 ﻿using EFCoreModelApp;
 
 var context = new CompanyContext();
+
+if (context.Departments.Count() == 0)
+{
+    context.Departments.Add(
+        new Department
+        {
+            Name = "IT",
+            Employees = new List<Employee>
+            {
+                new Employee()
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    HireDate = DateTime.Now,
+                },
+                new Employee()
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    HireDate = DateTime.Now,
+                }
+            }
+        }
+    );
+    context.SaveChanges();
+}
+
+
+var employees = context.Employees.Where(e => e.Department.Name == "IT");
+Console.WriteLine($"Employees in engineering: {employees.Count()}");
